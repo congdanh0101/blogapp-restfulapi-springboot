104431aceb26336c01f8e810c7cb0cd7812e5854
congdanh0101 <congdanh010101>
2022-08-01 01:22:18 +0700

    add jwt

--- src/main/java/springboot/webapp/config/SecurityConfig.java
+++ src/main/java/springboot/webapp/config/SecurityConfig.java
@@ -3,14 +3,19 @@ package springboot.webapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import springboot.webapp.security.CustomUserDetailService;
+import springboot.webapp.security.JwtAuthenticationEntryPoint;
+import springboot.webapp.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +24,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomUserDetailService customUserDetailService;
 
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+		http.csrf().disable().authorizeHttpRequests().antMatchers("/api/v1/auth/login").permitAll().anyRequest()
+				.authenticated().and().exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 	}
 
@@ -36,6 +51,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
---
+++ src/main/java/springboot/webapp/payloads/JwtAuthRespone.java
@@ -0,0 +1,14 @@
+package springboot.webapp.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthRespone {
+
+	private String token;
+
+}
